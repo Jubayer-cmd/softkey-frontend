d2f6dd8b2dffc0f266b0a16f8b317ac8e38f7512
Jubayer-cmd <sheikhabujubayer@gmail.com>
2023-12-29 12:11:47 +0600

    database url changed

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,8 +1,6 @@
 export const getBaseUrl = (): string => {
   return (
     process.env.NEXT_PUBLIC_API_BASE_URL ||
-    //'https://softkey-backend.vercel.app/api/v1'
-    // 'http://192.168.0.31:9000/api/v1'
-    'http://localhost:9000/api/v1'
+    'https://softkey-backend-mticc823u-jubayer-cmd.vercel.app/api/v1'
   );
 };
